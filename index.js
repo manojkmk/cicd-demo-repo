ecca87e12c7899c700e92befc93bd0e8bd8de6e4
Manoj Kumar K <kmkmanoj1991@gmail.com>
2025-09-14 19:20:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200);
-  res.end('Hello from CI/CD Pipeline! Updated version mbu.');
+  res.end('Hello from CI/CD Pipeline! Updated version mbu');
+   res.end('welcome to expert talk on devops');
 });
 
 server.listen(3000, '0.0.0.0', () => {
