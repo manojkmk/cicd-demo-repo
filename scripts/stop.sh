a242fa48d92b6322966bf9816d9fe728a38ec74a
Manoj Kumar K <kmkmanoj1991@gmail.com>
2025-09-14 18:25:27 +0530

    Update stop.sh

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 
-# Get the PID of the running Node.js app
-PID=$(pgrep -f 'node index.js')
-
-if [ -n "$PID" ]; then
-    echo "Stopping existing node process with PID: $PID"
-    kill -9 $PID
-else
-    echo "No running node process found."
-fi
+# Stop the application via PM2
+pm2 delete node-app || true
