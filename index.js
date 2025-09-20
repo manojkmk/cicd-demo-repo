81fbd6d5d5ef245e9f99945f37ff60999e9eb878
Manoj Kumar K <kmkmanoj1991@gmail.com>
2025-09-20 07:35:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200);
-  res.end('Hello from CI/CD Pipeline-surendra welcome to MITS');
+  res.end('Hello from CI/CD Pipeline-surendra welcome to MITS college');
 
 });
 
